0b03927194107a7e7fcc8e7226555c463c3636ad
kiketion <kiketionje@gmail.com>
2021-08-28 21:40:43 -0500

    details...

--- src/components/TableCoins.js
+++ src/components/TableCoins.js
@@ -4,16 +4,18 @@ import CoinRow from './CoinRow';
 const titles = ['#', 'Coin', 'Price', 'Price Change', '24h Volume'];
 
 const TableCoins = ({ coins, search }) => {
-  const filteredCoins = coins.filter((coin) =>
-    coin.name.toLowerCase().includes(search.toLowerCase())
+  const filteredCoins = coins.filter(
+    (coin) =>
+      coin.name.toLowerCase().includes(search.toLowerCase()) |
+      coin.symbol.toLowerCase().includes(search.toLowerCase())
   );
 
   return (
     <table className='table table-dark mt-4 table-hover'>
       <thead>
         <tr>
-          {titles.map((title) => (
-            <td>{title}</td>
+          {titles.map((title, index) => (
+            <td key={index}>{title}</td>
           ))}
         </tr>
       </thead>
